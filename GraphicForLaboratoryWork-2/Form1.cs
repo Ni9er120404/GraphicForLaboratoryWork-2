05bccd20c8c62d34ac1304ef3582d8c453c715a5
Даниил Лямин <Niger120404@yandex.ru>
2022-10-11 14:02:43 +0300

    Добавьте файлы проекта.

---
+++ GraphicForLaboratoryWork-2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GraphicForLaboratoryWork_2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			const int N = 3500;
+			for (int x = 1; x < N; x++)
+			{
+				int y = (int)Math.Pow(x, 2);
+				chart1.Series[0].Points.AddXY(x, y);
+				int y1 = (int)Math.Pow(3*x, 2);
+				chart1.Series[1].Points.AddXY(x, y1);
+
+			}
+		}
+
+		private void Chart1_Click(object sender, EventArgs e)
+		{
+
+
+		}
+	}
+}
